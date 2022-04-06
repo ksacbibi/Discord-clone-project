eae063eb7bdd4f6310062e728e4f69b817ec2c1c
ksacbibi <34385626+ksacbibi@users.noreply.github.com>
2022-04-06 09:44:22 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="e13369ff-79b6-45d3-b486-a15e49de56db"
+            userName="ksacbibi"
+            userSecret="123123"
+        />
+    )
 }
 
 export default App;
