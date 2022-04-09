ef46d31e1c92c606d97cc74870ea37af5894eba4
ksacbibi <34385626+ksacbibi@users.noreply.github.com>
2022-04-09 16:22:29 -0400

    Make ChatFeed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
 
 const App = () => {
@@ -9,6 +11,7 @@ const App = () => {
             projectID="e13369ff-79b6-45d3-b486-a15e49de56db"
             userName="ksacbibi"
             userSecret="123123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps}/>}
         />
     )
 }
