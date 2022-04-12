6ad1f67d3ca02d8913b70ecd9d178f641b64dd17
ksacbibi <34385626+ksacbibi@users.noreply.github.com>
2022-04-11 20:30:17 -0400

    Message form update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 
 import './App.css';
 
 const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
     return (
         <ChatEngine 
             height="100vh"
             projectID="e13369ff-79b6-45d3-b486-a15e49de56db"
-            userName="ksacbibi"
-            userSecret="123123"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps}/>}
         />
     )
